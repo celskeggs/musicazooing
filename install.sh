73186c31f738c3d75b718f7f4966ada8fdecec5c
Sam Dukhovni <dukhovni@duxovni.org>
2017-09-01 12:55:47 -0400

    Make installer more complete and verbose

--- install.sh
+++ install.sh
@@ -5,6 +5,16 @@ HERE=$(pwd)
 
 . ./config.env
 
+
+echo "=> Installing debian packages"
+sudo apt-get install python3-pip nginx redis-server mplayer
+
+
+echo "=> Installing pip packages"
+pip3 install --user --upgrade cherrypy youtube-dl redis pyserial
+
+
+echo "=> Creating systemd services"
 mkdir -p $HOME/.config/systemd/user
 
 for x in webserver.service downloader.service player.service button.service
@@ -12,11 +22,15 @@ do
 	cat $HERE/$x | sed "s|DIR|$HERE|g" >$HOME/.config/systemd/user/$x
 done
 
+
+echo "=> Setting up nginx web server"
 cat $HERE/nginx-site | sed "s|DIR|$MZ_LOCATION|" | sudo tee /etc/nginx/sites-available/musicazoo > /dev/null
+sudo ln -sf /etc/nginx/sites-available/musicazoo /etc/nginx/sites-enabled/musicazoo
 
-pip3 install --user --upgrade cherrypy youtube-dl redis pyserial
 
-sudo systemctl start redis-server
+echo "=> Starting systemd services"
+sudo systemctl restart redis-server nginx
+sudo loginctl enable-linger $USER
 systemctl daemon-reload --user
 systemctl enable --user webserver downloader player
 systemctl start --user webserver downloader player
