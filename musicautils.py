7780eb797ab37a786c380c0625715a1f6d2637d9
Faye Duxovni <duxovni@duxovni.org>
2019-09-30 17:12:08 -0400

    go back to defining DATA_DIR in musicautils

--- musicautils.py
+++ musicautils.py
@@ -1,10 +1,12 @@
 import os
 import re
 
+DATA_DIR = os.getenv("MZ_DATA_DIR")
+
 def sanitize(ytid):
 	print("given", ytid)
 	return re.sub("[^-a-zA-Z0-9_:]", "?", ytid)
 
 def path_for(ytid):
-	return os.path.join(os.getenv("MZ_DATA_DIR"), sanitize(ytid) + ".mp4")
+	return os.path.join(DATA_DIR, sanitize(ytid) + ".mp4")
 
